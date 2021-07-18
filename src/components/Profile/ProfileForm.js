53405a138bdfe3d134745093c1344c19c12fc061
Samira Milne <milne.samira@GMAIL.COM>
2021-07-18 17:13:02 -0500

    added redirect for login + new pw

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,8 +1,10 @@
 import {useRef, useContext} from 'react';
+import { useHistory } from "react-router";
 import AuthContext from '../../store/auth-context';
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
+  const history= useHistory();
   const newPasswordInputRef= useRef();
   const authCtx= useContext(AuthContext);
 
@@ -21,7 +23,7 @@ const ProfileForm = () => {
        'Content-Type': 'application/json'
      }
    }).then(res=>{
-     
+     history.replace('/');
    })
   }
  
